bd08df54d4f7f3fb88815467033b4a809f078d4c
lucaslg <lucaslg@users.noreply.github.com>
2014-11-02 13:37:04 +0100

    Ajout des baleines et des scripts associés

---
+++ scripts/Whale.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whale : MonoBehaviour {
+
+	[HideInInspector]
+	public bool Played = false;
+	[HideInInspector]
+	public bool Playing = false;
+
+	private Animation WhaleAnimation;
+
+	void Start()
+	{
+		WhaleAnimation = GetComponent<Animation> ();
+	}
+
+	void Update()
+	{
+		Playing = WhaleAnimation.isPlaying;
+	}
+}
---
+++ scripts/Whale_trigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whale_trigger : MonoBehaviour {
+
+	public GameObject Whale;
+
+	private Whale WhaleClass;
+	//public AudioSource SoundSource;
+
+	// Use this for initialization
+	void Start () {
+		Whale.GetComponent<MeshRenderer> ().enabled = false;
+		WhaleClass = Whale.GetComponent<Whale> ();
+		//SoundSource.Stop ();
+	}
+
+	void OnTriggerEnter()
+	{
+		if (!WhaleClass.Played && !WhaleClass.Playing)
+		{
+			WhaleClass.Played = true;
+			Whale.GetComponent<MeshRenderer> ().enabled = true;
+			Whale.GetComponent<Animation> ().Play ();
+		
+			//SoundSource.Play ();
+		}
+
+
+	}
+}
